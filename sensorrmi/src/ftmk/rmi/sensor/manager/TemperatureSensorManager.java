588ab072d9e9378e2188033005180e3e51a1ce50
Shafiqah Nur Athirah Binti Hassan <shafiqahnurathirah@gmail.com>
2021-04-23 11:04:14 +0800

    Exercise 6.n: Overriding Inherited Method

--- sensorrmi/src/ftmk/rmi/sensor/manager/TemperatureSensorManager.java
+++ sensorrmi/src/ftmk/rmi/sensor/manager/TemperatureSensorManager.java
@@ -23,21 +23,36 @@ public class TemperatureSensorManager extends UnicastRemoteObject implements Tem
 
 		return 35;
 	}
-	
-	//method to load data from table 1
-		private HashMap<String, Integer> loadDataTemperature() {
-			HashMap<String, Integer> temperaturePerDay = new HashMap<>();
 
-			// Add elements to a HashMap - use put()
-			temperaturePerDay.put("Monday", 32);
-			temperaturePerDay.put("Tuesday", 31);
-			temperaturePerDay.put("Wednesday", 33);
-			temperaturePerDay.put("Thursday", 35);
-			temperaturePerDay.put("Friday", 36);
-			temperaturePerDay.put("Saturday", 33);
-			temperaturePerDay.put("Sunday", 33);
+	// method to load data from table 1
+	private HashMap<String, Integer> loadDataTemperature() {
 
-			return temperaturePerDay;
-		}
+		// Create HasnMap
+		HashMap<String, Integer> temperaturePerDay = new HashMap<>();
+
+		// Hashmap #1 - Add elements to a HashMap - use put(key, value)
+		// key = String day, value = int temperature
+		temperaturePerDay.put("Monday", 32);
+		temperaturePerDay.put("Tuesday", 31);
+		temperaturePerDay.put("Wednesday", 33);
+		temperaturePerDay.put("Thursday", 35);
+		temperaturePerDay.put("Friday", 36);
+		temperaturePerDay.put("Saturday", 33);
+		temperaturePerDay.put("Sunday", 33);
+
+		return temperaturePerDay;
+	}
+
+	@Override
+	public int getTemperatureByDay() throws RemoteException {
+
+		// Create HasnMap
+		HashMap<String, Integer> temperature = loadDataTemperature();
+
+		// HashMap #2 - Access HashMap elements - use get(key)
+		// key = String day, value = int temperature
+		int temperatureValue = temperature.get("Friday");
+		return temperatureValue;
+	}
 
 }
