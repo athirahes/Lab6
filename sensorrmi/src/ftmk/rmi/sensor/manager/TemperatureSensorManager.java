92eb893a480cf5da6b987e1ec1aaae06403bc6b1
Shafiqah Nur Athirah Binti Hassan <shafiqahnurathirah@gmail.com>
2021-04-23 09:24:24 +0800

    Commit #5.n - Exercise 5: Load Temperature Data

--- sensorrmi/src/ftmk/rmi/sensor/manager/TemperatureSensorManager.java
+++ sensorrmi/src/ftmk/rmi/sensor/manager/TemperatureSensorManager.java
@@ -2,6 +2,7 @@ package ftmk.rmi.sensor.manager;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
 
 import ftmk.rmi.sensor.TemperatureSensor;
 
@@ -22,5 +23,21 @@ public class TemperatureSensorManager extends UnicastRemoteObject implements Tem
 
 		return 35;
 	}
+	
+	//method to load data from table 1
+		private HashMap<String, Integer> loadDataTemperature() {
+			HashMap<String, Integer> temperaturePerDay = new HashMap<>();
+
+			// Add elements to a HashMap - use put()
+			temperaturePerDay.put("Monday", 32);
+			temperaturePerDay.put("Tuesday", 31);
+			temperaturePerDay.put("Wednesday", 33);
+			temperaturePerDay.put("Thursday", 35);
+			temperaturePerDay.put("Friday", 36);
+			temperaturePerDay.put("Saturday", 33);
+			temperaturePerDay.put("Sunday", 33);
+
+			return temperaturePerDay;
+		}
 
 }
